888f612414bfb416174a4e58992e925cda387f5f
JulienOza <julien.ozarowski@gmail.com>
2023-10-04 16:24:02 +0200

    mobile version without flipcards

--- index.js
+++ index.js
@@ -39,19 +39,17 @@ btnNear.onclick = function() {
         near[i].remove();
     }
 }
-var sidenav = document.getElementById("mySidenav");
-var openBtn = document.getElementById("openBtn");
-var closeBtn = document.getElementById("closeBtn");
+const sidenav = document.getElementById("mySidenav");
+const openBtn = document.getElementById("openBtn");
+const closeBtn = document.getElementById("closeBtn");
 
 openBtn.onclick = openNav;
 closeBtn.onclick = closeNav;
 
-/* Set the width of the side navigation to 250px */
 function openNav() {
   sidenav.classList.add("active");
 }
 
-/* Set the width of the side navigation to 0 */
 function closeNav() {
   sidenav.classList.remove("active");
 }
